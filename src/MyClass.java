f02129e8bca8ffacbaa834a0d123bdf9e861e20c
Alexis Abner <alexisjuliaabner@gmail.com>
2022-06-30 18:58:59 -0600

    update files

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,5 +1,8 @@
 public class MyClass {
     public static void main(String[] args) {
-
+        MySubClass mySubClass = new MySubClass();
+        mySubClass.superClassMethod();
+        System.out.println(mySubClass.superStr);
+        mySubClass.subClassMethod();
     }
 }
--- src/MySubClass.java
+++ src/MySubClass.java
@@ -1,3 +1,10 @@
-public class MySubClass {
+public class MySubClass extends MySuperClass{
+    public MySubClass() {
+        super("");
+        System.out.println("sub class constructor");
+    }
 
+    public void subClassMethod() {
+        System.out.println("sub class method");
+    }
 }

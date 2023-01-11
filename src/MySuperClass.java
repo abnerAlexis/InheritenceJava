4242299a02177e5f0662805e2ed993318da3fc2a
Jen <68366562+abnerAlexis@users.noreply.github.com>
2023-01-10 17:30:55 -0700

    Update MySuperClass.java

--- src/MySuperClass.java
+++ src/MySuperClass.java
@@ -1,9 +1,11 @@
 public class MySuperClass {
     String superStr = "Super class string";
+    
     public MySuperClass(String constructorStr) {
         System.out.println("super class constructor");
         System.out.println(constructorStr);
     }
+    
     public void superClassMethod(){
         System.out.println("Super class method.");
     }
